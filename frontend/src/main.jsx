6d91a32815284be6012dc79cde485742d8b7d1ba
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-09-13 15:52:12 +0800

    Frontend Initialization: - organize frontend directory with needed folders - setup axios (api.js) and constants

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-router-dom";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
